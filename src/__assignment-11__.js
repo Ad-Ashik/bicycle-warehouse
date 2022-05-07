65b80b4c54d42610def4df5c24ff56e45eee729e
AIA <adashik585@gmail.com>
2022-05-08 01:03:42 +0600

    Database creat and load data mongo

--- src/__assignment-11__.js
+++ src/__assignment-11__.js
@@ -3,5 +3,7 @@
 * create header & navbar
 * two extra sections add Features and Accessories
 * six items in the inventory items section
-* click open single cycle id and create private route.  
+* click open single cycle id and create private route. 
+* create login page email/password and on social login 
+
  */
