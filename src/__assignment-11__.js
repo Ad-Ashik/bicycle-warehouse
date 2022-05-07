8635158af31c403d0765aad80e076914377acaab
AIA <adashik585@gmail.com>
2022-05-08 01:04:46 +0600

    Database creat and load data mongodb

--- src/__assignment-11__.js
+++ src/__assignment-11__.js
@@ -5,5 +5,5 @@
 * six items in the inventory items section
 * click open single cycle id and create private route. 
 * create login page email/password and on social login 
-
+* Database creat and load data mongodb
  */
